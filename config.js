ddbbca57091eec9564cef141c77b93eb83f9a35e
Annika Wollschläger <annika.wollschlaeger@tum.de>
2021-02-07 09:56:02 +0100

    add singularity article

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ const config = {
       '/transformation',
       '/forwardkinematics',
       '/inversekinematics',
+      '/singularity',
       '/impedancecontrol',
     ],
     // collapsedNav: [
---
+++ content/singularity.md
@@ -0,0 +1,5 @@
+---
+title: "Singularity"
+metaTitle: "Singularity | Robotics Explained"
+metaDescription: "Singularities in robotics"
+---
